f4812e8fe8662747b0f08b678a66af3650c8d30a
Tilo K <tiloklarenbeek@gmail.com>
2024-02-11 22:55:51 +0100

    feat: basic creation of messages

--- lib/chatapp_web/controllers/page_controller.ex
+++ lib/chatapp_web/controllers/page_controller.ex
@@ -1,14 +1,45 @@
 defmodule ChatappWeb.PageController do
   use ChatappWeb, :controller
 
+  @messages_agent_name __MODULE__
+
+  def child_spec(_args) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, []},
+      type: :worker,
+      restart: :permanent,
+      shutdown: 500
+    }
+  end
+
+  def start_link do
+    Agent.start_link(fn -> [] end, name: @messages_agent_name)
+  end
+
+  def messages do
+    Agent.get(@messages_agent_name, & &1)
+  end
+
+  def add_message(message) do
+    Agent.update(@messages_agent_name, &(&1 ++ [message]))
+  end
+
   def home(conn, _params) do
-    msg = [
-      %{author: "test author", message: "This is a test message"}
-    ]
-    render(conn, :home,messages: msg, layout: false)
+    render(conn, :home, messages: messages(), layout: false)
   end
 
   def send(conn, params) do
-    
+    add_message(%{author: "test author", message: params["message"]})
+
+    conn
+    |> put_status(200)
+    |> json(%{msg: params["message"]})
+  end
+
+  def get_messages(conn, _params) do
+    conn
+    |> put_status(200)
+    |> json(messages())
   end
 end
--- lib/chatapp_web/router.ex
+++ lib/chatapp_web/router.ex
@@ -6,7 +6,7 @@ defmodule ChatappWeb.Router do
     plug :fetch_session
     plug :fetch_live_flash
     plug :put_root_layout, html: {ChatappWeb.Layouts, :root}
-    plug :protect_from_forgery
+    # plug :protect_from_forgery
     plug :put_secure_browser_headers
   end
 
@@ -18,7 +18,6 @@ defmodule ChatappWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
-    post "/send", PageController, :send
   end
 
   # Other scopes may use custom stacks.
